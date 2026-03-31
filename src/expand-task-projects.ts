61b2c086b5e5c383dd0ebeb696df99c0a3a51523
Andy Frost <andyjfrost@gmail.com>
2026-03-30 22:26:33 -0700

    Project field expansion

--- src/expand-task-projects.ts
+++ src/expand-task-projects.ts
@@ -3,17 +3,44 @@ import { log } from "./logger";
 
 export function expandTaskProjects(): void {
   const projectsRoot = document.querySelector<HTMLElement>(".TaskProjects");
-  if (!projectsRoot) {
+  const taskPane = document.querySelector<HTMLElement>(".TaskPane");
+  if (!projectsRoot && !taskPane) {
     return;
   }
 
-  const toggles = $$(".TaskProjectWithCustomPropertyRows-toggleButton", projectsRoot)
-    .filter((el) => el.getAttribute("aria-label") === "Expand fields")
-    .filter((el) => !el.dataset.asanaExpanderClicked);
+  const projectToggles = projectsRoot
+    ? $$(".TaskProjectWithCustomPropertyRows-toggleButton", projectsRoot)
+        .filter((el) => el.getAttribute("aria-label") === "Expand fields")
+        .filter((el) => !el.dataset.asanaExpanderClicked)
+    : [];
+  const loadMoreToggles = $$(".TaskPaneFields-loadMore", taskPane ?? document)
+    .filter((el) => !el.dataset.asanaExpanderClicked)
+    .filter((el) => el.textContent?.includes("Show inherited fields"));
 
-  for (const toggle of toggles) {
+  for (const toggle of projectToggles) {
     toggle.dataset.asanaExpanderClicked = "true";
     log("Expand Task Projects", toggle);
     toggle.click();
+  }
+
+  for (const toggle of loadMoreToggles) {
+    toggle.dataset.asanaExpanderClicked = "true";
+    log("Expand Task Project Fields", toggle);
+    toggle.click();
+    toggle.blur();
+  }
+
+  const customFieldToggles = projectsRoot
+    ? $$(".TaskPaneFields-showMoreCustomFields", projectsRoot).filter(
+        (el) => !el.dataset.asanaExpanderClicked,
+      )
+        .filter((el) => !el.textContent?.includes("Hide custom fields"))
+    : [];
+
+  for (const toggle of customFieldToggles) {
+    toggle.dataset.asanaExpanderClicked = "true";
+    log("Expand Task Project Custom Fields", toggle);
+    toggle.click();
+    toggle.blur();
   }
 }
